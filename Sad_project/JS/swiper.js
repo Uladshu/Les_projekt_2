b50cbd4d03b6b831bf26047da1d4a2160a3996c9
Uladshu <124121304+Uladshu@users.noreply.github.com>
2023-02-23 19:35:51 +0100

    Sad_poject

---
+++ Sad_project/JS/swiper.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   $('.slider').slick({
+   slidesToShow: 3,
+   adaptiveHeight:true,
+   speed: 1500,
+   infinite:true,
+   autoplay:true,
+   autoplaySpeed:6000,
+   waitForAnimate:true,
+   });
+});
